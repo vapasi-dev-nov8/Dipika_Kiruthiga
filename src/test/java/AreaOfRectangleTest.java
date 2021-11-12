1e6fdbcb0d6353ef3dd8f33b24bde31307e89509
Kiruthiga <kiruthiga.career@gmail.com>
2021-11-12 12:02:16 +0530

    Modified AreaofRectangleTest

--- src/test/java/AreaOfRectangleTest.java
+++ src/test/java/AreaOfRectangleTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AreaOfRectangleTest {
     @Test
     void shouldReturnAreaOfRectangle(){
-        assertEquals(7::wq2,AreaOfRectangle.calculateArea(12,6));
+        assertEquals(7,AreaOfRectangle.calculateArea(12,6));
     }
 
 
